a63ef4c3f9bc11831c759331fb470a4d733c68db
Nikita Moiseychenko <nik.moiseichenko.06@gmail.com>
2024-10-15 23:34:57 +0500

    Add an option to leave a review(a button and a popup in Movie.html) Added links into FAQ

---
+++ reviewPopup.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function(){
+  const reviewToggleButton=document.getElementById("Review");
+  const reviewPopup=document.getElementById("review-popup")
+    const overlay=document.getElementById("overlay")
+  reviewToggleButton.addEventListener("click", ()=> {
+      if(reviewPopup.style.display === "none" || reviewPopup.style.display===""){
+          reviewPopup.style.display="block";
+          overlay.style.display="block"
+      } else {
+          reviewPopup.style.display="none";
+      }
+  })
+    overlay.addEventListener("click",()=>{
+        reviewPopup.style.display="none";
+        overlay.style.display="none"
+    })
+
+})
+function updateRating(star_amount){
+    let stars=document.querySelectorAll(".star")
+    for(let i=0;i<star_amount;i++){
+        stars[i].className="star "+"checked "+"fs-1"
+    }
+    for(let i=star_amount;i<5;i++){
+        stars[i].className="star "+"fs-1"
+    }
+}
