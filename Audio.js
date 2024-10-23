a7e6d31255f30be8ff6329d8fc1c8d0e30c5e26a
Nikita Moiseychenko <nik.moiseichenko.06@gmail.com>
2024-10-23 12:44:51 +0500

    Store muted state in local storage, remove .no-transitions, add tooltips to mute and change theme buttons

--- Audio.js
+++ Audio.js
@@ -1,8 +1,14 @@
 const muteButton=document.getElementById("toggleSound");
-let soundOn=true;
+let soundOn=localStorage.getItem("muted")==="true" || localStorage.getItem("muted")===null
+if(!soundOn){
+    muteButton.classList.toggle('bi-volume-up-fill');
+    muteButton.classList.toggle('bi-volume-mute-fill')
+}
 muteButton.addEventListener("click", function() {
     this.classList.toggle('bi-volume-up-fill')
     soundOn = !this.classList.toggle('bi-volume-mute-fill');
+    localStorage.setItem("muted", soundOn)
+
 })
 function play(audio){
     if(soundOn) {
